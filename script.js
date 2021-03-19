bf09b4ab07c3b2860d5913b9a185312e2fe3e978
Whitely <matthewwhitely77@gmail.com>
2021-03-19 12:15:24 -0400

    added a random image generator need to figure out how to size the images to be the exact same!

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const subMenu = document.querySelector(".subMenu");
 const harperNav = document.querySelector("#harper");
 const modal = document.querySelectorAll(".modal");
 const overlay = document.querySelectorAll(".overlay");
-console.log(hamburgerMenu);
-console.log(subMenu);
-console.log(harper);
-console.log(modal);
-console.log(overlay);
+const ranImage = document.querySelector(".ranImg");
+const harperButton = document.querySelector("#harperButton");
+// console.log(ranImage);
+// console.log(harperButton);
+// console.log(hamburgerMenu);
+// console.log(subMenu);
+// console.log(harper);
+// console.log(modal);
+// console.log(overlay);
 
 hamburgerMenu.addEventListener("click", function () {
   subMenu.classList.add("subMenu1");
@@ -24,3 +28,14 @@ const closeModal = function () {
 
 harper.addEventListener("click", openModal);
 overlay[0].addEventListener("click", closeModal);
+
+//Randomize on the harper button the top image
+
+const randomizeImage = function () {
+  let rImg = Math.floor(Math.random() * 8);
+
+  ranImage.src = `harper-designAndAssets/assets/home-image-${rImg}.jpg`;
+};
+
+harperButton.addEventListener("click", randomizeImage);
+// randomizeImage();
